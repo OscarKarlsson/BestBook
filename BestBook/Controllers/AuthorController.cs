0cd43bc4ed4f9b1ed873601673f9d513a99b18dd
EmanuelKarlsson <emanuel.karlsson1@edu.varberg.se>
2021-06-10 11:52:18 +0200

    Add book, add author osv..

---
+++ BestBook/Controllers/AuthorController.cs
@@ -0,0 +1,33 @@
+﻿using BestBook.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BestBook.Controllers
+{
+    public class AuthorController : Controller
+    {
+        public BookContext Context { get; set; }
+        public AuthorController(BookContext context)
+        {
+            this.Context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AddAuthor()
+        {
+            TempData["name"] = "Stephen";
+            return View();
+        }
+        public IActionResult SearchAuthor(string name)
+        {
+            var authors = Context.Authors.Where(a => a.Name.Contains(name));
+            return View(authors);
+        }
+            
+    }
+}
