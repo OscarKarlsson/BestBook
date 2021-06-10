d32e2ca37b0afb85284302ece3dfce04c2f005ee
EmanuelKarlsson <emanuel.karlsson1@edu.varberg.se>
2021-06-10 09:20:40 +0200

    Adminmeny, Add book och BookController

---
+++ BestBook/Controllers/BookController.cs
@@ -0,0 +1,38 @@
+﻿using BestBook.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BestBook.Controllers
+{
+    public class BookController : Controller
+    {
+        public BookContext Context { get; set; }
+        public BookController(BookContext context)
+        {
+            this.Context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                Context.Books.Add(book);
+                Context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(book);
+        }
+    }
+}
